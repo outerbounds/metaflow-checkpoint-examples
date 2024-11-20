840ea97e4d2890b4b17806575f70cab9c89668b1
Valay Dave <valaygaurang@gmail.com>
2024-11-20 11:34:22 -0800

    [hf hub] moar examples

--- hf_registry/hub_deco_flow.py
+++ hf_registry/hub_deco_flow.py
@@ -15,7 +15,7 @@ import hashlib
 
 from metaflow import FlowSpec, current, step, huggingface_hub
 
-HUB_PYPI_PARAMETES = {
+HUB_PYPI_PARAMETERS = {
     "packages": {"huggingface-hub": "0.16.4", "omegaconf": "2.4.0.dev3"},
     "python": "3.11.5",
 }
@@ -42,7 +42,7 @@ class HFCachedModelFlow(FlowSpec):
         type=str,
     )
 
-    @pypi(**HUB_PYPI_PARAMETES)
+    @pypi(**HUB_PYPI_PARAMETERS)
     @huggingface_hub
     @step
     def start(self):
@@ -56,7 +56,7 @@ class HFCachedModelFlow(FlowSpec):
 
         self.next(self.in_step_load)
 
-    @pypi(**HUB_PYPI_PARAMETES)
+    @pypi(**HUB_PYPI_PARAMETERS)
     @huggingface_hub(load=["google-bert/bert-base-uncased"])
     @step
     def in_step_load(self):
@@ -69,7 +69,42 @@ class HFCachedModelFlow(FlowSpec):
             "contents of the directory : ",
             os.listdir(model_path),
         )
+        self.next(self.another_step)
+    
+    @pypi(**HUB_PYPI_PARAMETERS)
+    @huggingface_hub(load=[
+        {
+            "repo_id": "bert-base-uncased",
+            "allow_patterns": ["*.json", "tokenizer.txt"],
+            "repo_type": "model"
+        },
+        {
+            "repo_id": "t5-small",
+            "allow_patterns": ["config.*", "tokenizer.*"],
+            "force_download": True # setting `force_download` will ensure that we bust cache 
+        }
+    ])
+    @step
+    def another_step(self):
+        import os
+
+        model_path = current.huggingface_hub.loaded["t5-small"]
+        print(
+            "t5-small Model is loaded in the directory",
+            model_path,
+            "contents of the directory : ",
+            os.listdir(model_path),
+        )
+        
+        model_path = current.huggingface_hub.loaded["bert-base-uncased"]
+        print(
+            "bert-base-uncased Model is loaded in the directory",
+            model_path,
+            "contents of the directory : ",
+            os.listdir(model_path),
+        )
         self.next(self.end)
+
 
     @model(load=["model_reference_foo"])
     @step
